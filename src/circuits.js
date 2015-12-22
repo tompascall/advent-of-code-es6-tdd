f5c460598b21ee1ecb902f1011241b95870b03be
Gulacsy-Toth <tompascall@gmail.com>
2015-12-22 19:48:17 +0100

    add OR computation

--- src/circuits.js
+++ src/circuits.js
@@ -1,8 +1,8 @@
 let circuits = {};
 
-circuits.createWireInputs = function (input) { // input form: <instruction> -> <token>\n
+circuits.createWires = function (input) { // input form: <instruction> -> <token>\n
   let rows = input.split('\n');
-  let wireInputs = {};
+  let wires = {};
   let reg = /(\S[\w\d\s]+\S)\s*\-\>\s*(\w+)/;
   for (let row of rows) {
     let instruction, wireName;
@@ -10,23 +10,49 @@ circuits.createWireInputs = function (input) { // input form: <instruction> -> <
     instruction = extracts[1];
     wireName = extracts[2];
     if (extracts) { 
-      wireInputs[ wireName ] = {
+      wires[ wireName ] = {
         instruction: instruction
       };
     }
   }
-  console.log(wireInputs);
-  return wireInputs;
+  console.log(wires);
+  return wires;
 }; 
 
-circuits.makePromises = function (wireInputs) {
-  let wireNames = Object.keys(wireInputs);
-  for (let wireName of wireNames) { // e.g. x
-    let wire = wireInputs[wireName];
-    let instruction = wire.instruction;
-    if ( !isNaN(instruction) ) {
-      circuits.makeSimpleValuePromise(wire);
+circuits.makePromises = function (wires) {
+  let wireNames = Object.keys(wires);
+  for (let wireName of wireNames) {
+    let wire = wires[wireName];
+    circuits.makePromise(wires, wire);
+  }
+};
+
+circuits.makePromise = function (wires, wire) {
+    if (typeof wire.value === 'undefined') {
+      let instruction = circuits.tokenizeInstruction(wire.instruction);
+      if ( instruction.length === 1) {  // it must be a simple value
+        circuits.makeSimpleValuePromise(wire);
+      }
+      else if (instruction[0] === 'NOT') {
+        circuits.make_NOT_Promise(wires, wire, instruction);
+      }
+      else if (instruction[1] === 'LSHIFT') {
+         circuits.make_LSHIFT_Promise(wires, wire, instruction);
+      }
+      else if (instruction[1] === 'RSHIFT') {
+         circuits.make_RSHIFT_Promise(wires, wire, instruction);
+      }
+      else if (instruction[1] === 'OR') {
+        circuits.make_OR_Promise(wires, wire, instruction);
+      }
     }
+};
+
+circuits.tokenizeInstruction = function (instruction) {
+  let reg = /([\d\w]+)/g;
+  let tokens = instruction.match(reg);
+  if (tokens) {
+    return tokens;
   }
 };
 
@@ -34,6 +60,54 @@ circuits.makeSimpleValuePromise = function (wire) {
   wire.value = new Promise( (resolve) => {
     resolve(Number(wire.instruction));
   })
+};
+
+circuits.make_NOT_Promise = function (wires, wire, instruction) {
+  wire.value = new Promise( (resolve) => {
+    if ( typeof wires[ instruction[1] ].value === 'undefined') {
+      circuits.makePromise(wires, wires[ instruction[1] ]);
+    }
+    wires[ instruction[1] ].value.then( (value) => {
+      resolve(~value);
+    }); 
+  });
+};
+
+circuits.make_LSHIFT_Promise = function (wires, wire, instruction) {
+  wire.value = new Promise( (resolve) => {
+    if ( typeof wires [ instruction[0] ].value === 'undefined') {
+      circuits.makePromise(wires, wires[ instruction[0] ]);
+    }
+    wires[ instruction[0] ].value.then( (value) => {
+      resolve( value << Number(instruction[2]) );
+    }); 
+  });
+};
+
+circuits.make_RSHIFT_Promise = function (wires, wire, instruction) {
+  wire.value = new Promise( (resolve) => {
+    if ( typeof wires [ instruction[0] ].value === 'undefined') {
+      circuits.makePromise(wires, wires[ instruction[0] ]);
+    }
+    wires[ instruction[0] ].value.then( (value) => {
+      resolve( value >> Number(instruction[2]) );
+    }); 
+  });
+};
+
+circuits.make_OR_Promise = function (wires, wire, instruction) {
+  wire.value = new Promise( (resolve) => {
+    if ( typeof wires [ instruction[0] ].value === 'undefined') {
+      circuits.makePromise(wires, wires[ instruction[0] ]);
+    }
+    if ( typeof wires [ instruction[2] ].value === 'undefined') {
+      circuits.makePromise(wires, wires[ instruction[2] ]);
+    }
+    let promises = [ wires[ instruction[0] ].value, wires[ instruction[2] ].value ];
+    Promise.all(promises).then(results => {
+      resolve(results[0] | results[1]);
+    });
+  });
 };
 
 export default circuits;
--- test/circuits.spec.js
+++ test/circuits.spec.js
@@ -11,19 +11,59 @@ describe('test day 7 puzzle: circuits', () => {
                   NOT y -> i
                   123 -> x
                   456 -> y`;
-    let wireInputs = circuits.createWireInputs(input);
-    expect(Object.keys(wireInputs).length).to.equal(8);
-    expect(wireInputs.y.instruction).to.equal('456');
-    expect(wireInputs.e.instruction).to.equal('x OR y');
+    let wires = circuits.createWires(input);
+    expect(Object.keys(wires).length).to.equal(8);
+    expect(wires.y.instruction).to.equal('456');
+    expect(wires.e.instruction).to.equal('x OR y');
   });
 
   it('should make promises from simple value instruction', () => {
-    let input = `123 -> x
-                 456 -> y`;
-    let wireInputs = circuits.createWireInputs(input);
-    circuits.makePromises(wireInputs);
-    return wireInputs.x.value.then( (value) => {
+    let input = '123 -> x';
+    let wires = circuits.createWires(input);
+    circuits.makePromises(wires);
+    return wires.x.value.then( (value) => {
       expect(value).to.equal(123);
     })
   });
+
+  it('should make promises from `NOT x` type instructions', () => {
+    let input = `NOT x -> y
+                 123 -> x`;
+    let wires = circuits.createWires(input);
+    circuits.makePromises(wires);
+    return wires.y.value.then( (value) => {
+      expect(value).to.equal(~123);
+    });
+  });
+
+  it('should make promises from `x LSHIFT` type instructions', () => {
+    let input = `x LSHIFT 2 -> y
+                 123 -> x`
+    let wires = circuits.createWires(input);
+    circuits.makePromises(wires);
+    return wires.y.value.then( (value) => {
+      expect(value).to.equal(123 << 2);
+    });
+  }); 
+
+  it('should make promises from `x RSHIFT` type instructions', () => {
+    let input = `x RSHIFT 2 -> y
+                 123 -> x`
+    let wires = circuits.createWires(input);
+    circuits.makePromises(wires);
+    return wires.y.value.then( (value) => {
+      expect(value).to.equal(123 >> 2);
+    });
+  }); 
+
+  it('should make promises from `x OR y` type instructions', () => {
+    let input = `x OR y -> z
+                 123 -> x
+                 456 -> y`
+    let wires = circuits.createWires(input);
+    circuits.makePromises(wires);
+    return wires.z.value.then( (value) => {
+      expect(value).to.equal(123 | 456);
+    });
+  }); 
 });
