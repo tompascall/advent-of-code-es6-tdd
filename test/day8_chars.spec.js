7336f0f0d87c961408e6daef74d3d039faa4c901
Gulacsy-Toth <tompascall@gmail.com>
2015-12-29 23:02:03 +0100

    solve 8th puzzle

---
+++ test/day8_chars.spec.js
@@ -0,0 +1,31 @@
+import Chars from '../src/day8_chars';
+
+describe('test 8th puzzle', () => {
+  let chars;
+  
+  it('should count the number of characters of code for string literals', () => {
+    let raw = String.raw`"asdf\""
+"a\"b\\c\xff"
+""
+"\\\"\xaa"`;
+    chars = new Chars(raw);
+    let counters = chars.counter();
+    expect(counters.code).to.equal(33);  
+  });
+
+  it('should count the number of characters in memory', () => {
+    let raw = String.raw`"asdf\""
+"a\"b\\c\xff"
+""
+"\\\"\xaa"`;
+    chars = new Chars(raw);
+    expect(chars.counter().mem).to.equal(14); 
+  });
+
+  it('should count the number of characters in string as input would be in memory', () => {
+    let raw = String.raw`"a\"b\\c\xff"`;
+    console.log(raw);
+    chars = new Chars(raw);
+    expect(chars.counter().invert).to.equal(22); // 13 + 9
+  });
+});
