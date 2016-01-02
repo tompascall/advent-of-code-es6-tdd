3354680000a0f6dff6f16e1599d9c586453cca04
Gulacsy-Toth <tompascall@gmail.com>
2016-01-02 17:20:30 +0100

    renamed files

--- src/first.js
+++
@@ -1,3 +0,0 @@
-export function echo (st) {
-  console.log(st);
-}
--- src/solutions_4.puzzle.js
+++
@@ -1,8 +0,0 @@
-import miner from './miner';
-
-let index = miner.getCoin({
-  secret: 'bgvyzdsv',
-  pattern: '^0{5}'
-}).index;
-
-console.log('Your mined number is', index);
